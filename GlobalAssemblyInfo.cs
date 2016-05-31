26723dc0be4795a7455a60419b3e27f0db8bed3e
Jared Dobson <jared.dobson@live.com>
2016-05-30 23:32:41 -0600

    1.0.0.3
    Fixes

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -3,6 +3,6 @@
 [assembly: AssemblyCompany("Martin Komischke")]
 [assembly: AssemblyProduct("DM.Emkay.S3")]
 [assembly: AssemblyCopyright("Copyright (c) 2016")]
-[assembly: AssemblyVersion("1.0.0.2")]
-[assembly: AssemblyFileVersion("1.0.0.2")]
+[assembly: AssemblyVersion("1.0.0.3")]
+[assembly: AssemblyFileVersion("1.0.0.3")]
 [assembly: AssemblyInformationalVersion("D5CAE5008CA9940FC96CC1DD89D46A143")]
